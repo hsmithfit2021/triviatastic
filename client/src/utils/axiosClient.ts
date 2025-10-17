7c24afc6fb4d5df5ceb1f3b7c2e362974f265c00
hsmithfit2021 <hsmith2021@my.fit.edu>
2025-10-17 15:11:49 -0400

    linking front/backend

--- client/src/utils/axiosClient.ts
+++ client/src/utils/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://ec2-54-226-187-157.compute-1.amazonaws.com:3000",
   headers: {
     "Content-Type": "application/json",
   },
